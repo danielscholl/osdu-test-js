9ba35a6ca29f84932f9ff2d01963a7a775e70d1c
Daniel Scholl <dascholl@microsoft.com>
2020-03-24 23:07:50 -0500

    Consolidation

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const replaceStorageCollections = require('./lib/facade/cosmosdb').replaceStorageCollections;
-const deleteAllIndices = require('./lib/facade/elasticsearch');
-const deleteAllMetadataBlobs = require('./lib/facade/blob').deleteAllMetadataBlobs;
-
 const myArgs = process.argv.slice(2);
-
-async function done() {
-  console.log('we seem to be finished');
-}
+const DataLoad = require('./lib/dataload.js');
+let dataLoad = new DataLoad();
 
 if (myArgs[0] === 'reset') {
-  deleteAllMetadataBlobs();
-  replaceStorageCollections()
-    .then(deleteAllIndices)
-    .then(done)
+  dataLoad.resetCollections()
+    .then(dataLoad.resetStorage())
+    .then(dataLoad.resetIndices())
+    .then(() => console.log('we seem to be finished'))
     .catch(console.error);
 } else {
   console.log('Pass the arg "reset" to cause the reset routine to execute.');
 }
+
+
+// const replaceStorageCollections = require('./lib/facade/cosmosdb').replaceStorageCollections;
+// const deleteAllIndices = require('./lib/facade/elasticsearch');
+// const deleteAllMetadataBlobs = require('./lib/facade/blob').deleteAllMetadataBlobs;
+
+// const myArgs = process.argv.slice(2);
+
+// async function done() {
+//   console.log('we seem to be finished');
+// }
+
+// if (myArgs[0] === 'reset') {
+//   deleteAllMetadataBlobs();
+//   replaceStorageCollections()
+//     .then(deleteAllIndices)
+//     .then(done)
+//     .catch(console.error);
+// } else {
+//   console.log('Pass the arg "reset" to cause the reset routine to execute.');
+// }
